88c3fdad0235d95c074632d6e89d96dd8ecaca50
kiralho <vinicius_torii@hotmail.com>
2018-10-09 16:23:47 +0000

    Created table Breed

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,4 @@
 class Store < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_161806) do
+ActiveRecord::Schema.define(version: 2018_10_09_162310) do
+
+  create_table "breeds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
