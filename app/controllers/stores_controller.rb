293d4b1dc34fb8c46b34d35c86cb232b0752339d
kiralho <vinicius_torii@hotmail.com>
2018-10-11 19:15:09 +0000

    Added routes and changed show views for breed and store so user can navigate through those views

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,10 @@
 class StoresController < ApplicationController
   def index
+    @stores = Store.all
   end
 
   def show
+    @store = Store.find(params[:id])
+    @deals = @store.deals
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'stores/index'
-  get 'stores/show'
+
   resources :breeds, only: [:index, :show]
+  resources :stores, only: [:index, :show]
+  get 'store/:id', to: 'stores#show'
+  get 'breed/:id', to: 'breeds#show'
+
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/about', to: 'pages#about'
   get 'about', to: 'pages#about'
