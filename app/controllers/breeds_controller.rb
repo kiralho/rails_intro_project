a4745b11027bd5813e7117247e4f189fe9af5134
kiralho <vinicius_torii@hotmail.com>
2018-10-11 16:01:42 +0000

    Updated breeds controller, index and show to display data

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,7 +1,11 @@
 class BreedsController < ApplicationController
   def index
+    @breeds = Breed.all
   end
 
   def show
+    @breeds = Breed.find(params[:id])
+    @sub_breeds = @breeds.sub_breeds.all
+    @deals = @breeds.deals.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'breeds/index'
-  get 'breeds/show'
+  resources :breeds, only: [:index, :show]
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/about', to: 'pages#about'
   get 'about', to: 'pages#about'
 
+  root to: 'breeds#index'
 end
 
